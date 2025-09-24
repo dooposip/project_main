f3f8ab5de82441f227b2d14e7639d9254674daea
Lsangsu <sangsu218@naver.com>
2025-09-24 15:19:47 +0900

    20250924

---
+++ travel/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM 적용
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # 블루 프린트 등록
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+    from . import models
+
+    return app
